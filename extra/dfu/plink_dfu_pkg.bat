7ae496468cb265e6ec57f1d47f1419287c7a0a09
AGreen <electronic.space.cat@gmail.com>
2023-03-07 19:41:08 -0600

    updated plink_dfu_pkg.bat to create version file

--- extra/dfu/plink_dfu_pkg.bat
+++ extra/dfu/plink_dfu_pkg.bat
@@ -2,14 +2,19 @@ ECHO OFF
 
 :: Version types:
 :: --application-version 1
-:: --application-version-string "1.0.0"
+:: --application-version-string "1.0.0"    <-- currently used for this project
 
 :: Modify the path defines as needed
 
 SET NRF_UTIL_LOC=..\..\..\
 SET HEX_FILE=..\..\..\plink-device-firmware\_Prod_makefile\nrf_plink_app.hex
 SET OUTPUT_NAME=plink
+SET VERSION=1.0.0
 
-%NRF_UTIL_LOC%\nrfutil.exe pkg generate --hw-version 52 --application-version-string "1.0.0" --application %HEX_FILE% --sd-req 0x103 --key-file private.key %OUTPUT_NAME%.zip
+%NRF_UTIL_LOC%\nrfutil.exe pkg generate --hw-version 52 --application-version-string "%VERSION%" --application %HEX_FILE% --sd-req 0x103 --key-file private.key %OUTPUT_NAME%.zip
 
-::PAUSE
+ECHO "%VERSION%"
+
+ECHO %VERSION% > version
+
+PAUSE
